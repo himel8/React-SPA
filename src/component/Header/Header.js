fa0d1935afac6bbc372481aafda8ee62e8ca31af
himel <himelkhan290@gmail.com>
2021-09-26 21:52:34 +0600

    final2

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,4 +1,4 @@
-import { faFighterJet, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import { faFighterJet } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Header.css'
